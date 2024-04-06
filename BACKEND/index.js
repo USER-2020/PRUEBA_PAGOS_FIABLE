1eb0de3dc799da092808319dcb06df3976b14af8
USER - 2020 <hackingetico2021@gmail.com>
2024-04-05 21:16:12 -0500

    fix: :test_tube: corss fix working

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 // Middleware para habilitar CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // Reemplaza localhost:5173 con el dominio de tu frontend
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Reemplaza localhost:5173 con el dominio de tu frontend
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
