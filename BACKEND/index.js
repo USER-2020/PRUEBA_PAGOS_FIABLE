7c5ad4f51a59fb2c512587f86bde00af60374b61
USER - 2020 <hackingetico2021@gmail.com>
2024-04-05 21:23:00 -0500

    fix:

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -7,15 +7,9 @@ import specialPriceProductUserRouter from './routes/specialpriceproduct.route.js
 import cors from 'cors';
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
-// Middleware para habilitar CORS
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Reemplaza localhost:5173 con el dominio de tu frontend
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+app.use(cors({ origin: '*' }));
 app.use('/api/v1', productRouter);
 app.use('/api/v1', userRouter);
 app.use('/api/v1', specialPriceProductUserRouter);
