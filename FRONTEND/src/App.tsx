ba19169c30e46e21f51af2bafdbb793476e6ce3a
USER-2020 <hackingetico2021@gmail.com>
2024-04-07 00:24:46 -0500

    changue in optimizate

--- FRONTEND/src/App.tsx
+++ FRONTEND/src/App.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Request from './components/request';
 import Response from './components/response';
